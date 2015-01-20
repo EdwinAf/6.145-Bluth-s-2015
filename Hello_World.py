63b42e8ec5769ca320a003b0ce54e40c0bc9d9b7
Edwin A <edwinjaf@mit.edu>
2015-01-19 22:51:04 -0500

    Original File
    Short Python Script

---
+++ Hello_World.py
@@ -0,0 +1,12 @@
+"#!/usr/bin/python"
+
+
+print "Hello World"
+
+
+for i in ['','h','i','','h','u','m','a','n',]:
+    print i
+    
+ANS = input("What time is it? ") 
+
+print ANS,'!',  'Nice to see you have a nice day, GOTTA GO!'
